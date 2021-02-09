fce51b7d8d3bf9eeb1009afb4ace5d7a094348f9
Michal Kacprzak <michal.kacprzak999@gmail.com>
2021-02-09 11:19:03 +0100

    create class Sounds for storing basic phrases which will assistant will  tell

---
+++ src/sounds.py
@@ -0,0 +1,26 @@
+AUDIO_FOLDER = "../audio_files"
+
+
+class Sounds:
+    GOOD_MORNING = "Good Morning"
+    GOOD_AFTERNOON = "Good Afternoon"
+    GOOD_EVENING = "Good Evening"
+    INCOMPREHENSIBLE_SOUND = "Sphinx could not understand audio can you repeat"
+    ERROR = "Sphinx error"
+    GOODBYE = "GoodBye"
+    JOKE = "I have a joke for u"
+    END = "See you again"
+    START = "Ready Set Go"
+    FINISH = "Finish"
+    INTRODUCE = "I am Jarvis"
+    SAD = "You seem sad"
+    NO_COMMEND = "No match for commend"
+    NO_NOTES = "There are no notes saved"
+    NO_CITY = "City Not Found"
+    BROWSE = "What do you want to browse"
+    GOOGLE = "What do you want to google"
+    WIKIPEDIA = "What do you want to search in wikipedia",
+    WEATHER = "Where do you want to check the weather"
+    HELP = "How may I help you"
+    CREATE_NOTE = "What note"
+    HOW_LONG = "How long"
